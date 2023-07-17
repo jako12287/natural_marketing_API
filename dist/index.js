7b55a2ea899d3c75acc9b46790ecaee78ebf2284
Johan Cortes <jako12287@gmail.com>
2023-07-17 15:54:14 -0500

    corrections

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const Products_routes_1 = __importDefault(require("./routes/Products.routes"));
 const Offers_routes_1 = __importDefault(require("./routes/Offers.routes"));
 const dataBase_1 = __importDefault(require("./dataBase"));
@@ -12,6 +13,7 @@ const Faq_routes_1 = __importDefault(require("./routes/Faq.routes"));
 const admin_routes_1 = __importDefault(require("./routes/admin.routes"));
 const Auth_routes_1 = __importDefault(require("./routes/Auth.routes"));
 const config_1 = require("./config");
+app_1.default.use((0, cors_1.default)());
 app_1.default.use(express_1.default.json());
 app_1.default.use(Auth_routes_1.default);
 app_1.default.use(Products_routes_1.default);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import app from "./app";
 import express from 'express'
+import cors from "cors"
 import routeProducts from "./routes/Products.routes";
 import routerOffers from "./routes/Offers.routes";
 import conectDB from "./dataBase";
@@ -7,6 +8,7 @@ import routerFaq from "./routes/Faq.routes";
 import routerAdmin from "./routes/admin.routes";
 import routerLogin from "./routes/Auth.routes";
 import { PORT } from "./config";
+app.use(cors())
 app.use(express.json())
 app.use(routerLogin);
 app.use(routeProducts);
