cffa32c23c36bb12c7a9693d68ab364c28a74795
Johan Cortes <jako12287@gmail.com>
2023-09-30 12:31:13 -0500

    setting for unknown urls new

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,9 @@ app.use(routerAdmin);
 app.use(routerSendEmail);
 app.use(routerVerifyToken);
 
-const publicPath = path.join(
-  __dirname,
-  "../src/assets/ssr/index.html"
-);
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(__dirname, "public", "../src/assets/ssr/index.html")); // Ajusta la ruta a tu archivo HTML principal.
+});
 
 conectDB().then(() => {
   app.listen(PORT, () => {
